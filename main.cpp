4e691b2f9edc63a892a82a4234326f8496ebe7b6
hx-zhangxiong <hellozhangxiong@163.com>
2022-11-22 13:56:38 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "partOne.h"
+
+int main()
+{
+    printf("this is main!\n");
+
+    printOK();
+
+    return 0;
+}
---
+++ partOne.cpp
@@ -0,0 +1,7 @@
+#include "partOne.h"
+#include <stdio.h>
+
+void printOK()
+{
+    printf("OK\n");
+}
